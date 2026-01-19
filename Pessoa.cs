e758dc1a6784c14f9fed696201e7439329d22719
lismarreiros <lismarreiros@hotmail.com>
2026-01-19 20:39:09 -0300

    classe Pessoa.cs

--- Pessoa.cs
+++ Pessoa.cs
@@ -2,5 +2,26 @@ namespace sis_biblioteca;
 
 public class Pessoa
 {
-    
+    public int id { get; set; }
+    public string nome { get; set; }
+    public List<Livro> livros { get; set; }
+
+    public Pessoa(int id, string nome)
+    {
+        this.id = id;
+        this.nome = nome;
+        livros = new List<Livro>();
+    }
+
+    public void PegarLivro(Livro livro)
+    {
+        livros.Add(livro);
+        Console.WriteLine($"{livro} adicionado com sucesso.");
+    }
+
+    public void DevolverLivro(Livro livro)
+    {
+        livros.Remove(livro);
+        Console.WriteLine($"{livro} removido com sucesso.");
+    }
 }
--- Program.cs
+++ Program.cs
@@ -2,12 +2,21 @@
 
 Livro greatGastby = new Livro(1, "Great Gatsby");
 Livro domCasmurro = new Livro(2, "Dom Casmurro");
-greatGastby.Emprestar("José");
+// greatGastby.Emprestar("José");
 
 // testes da classe Livro!
 Console.WriteLine($"{greatGastby.titulo} - status: {greatGastby.status}");
 Console.WriteLine($"{domCasmurro.titulo} - status: {domCasmurro.status}");
         
-domCasmurro.Emprestar("Camille");
+// domCasmurro.Emprestar("Camille");
 greatGastby.Devolver();
 Console.WriteLine($"locatário(?): {greatGastby.locatario}");
+
+Pessoa pessoa = new Pessoa(1, "Maria");
+
+pessoa.PegarLivro(greatGastby);
+pessoa.PegarLivro(domCasmurro);
+
+Console.WriteLine($"teste: {greatGastby.status}");
+    
+greatGastby.Emprestar("maria");
